9f98b20a41ff4aae96d14ec41ebc2417ce2934df
Tim Martin Berger <timmberg99@gmail.com>
2026-07-03 15:04:25 +0200

    01-js-app set up using compose

--- 01-js-app/app/server.js
+++ 01-js-app/app/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+
+var { getDB, userColName } = require('./database')
+
 var app = express();
 
-// Import MongoDB client node module
-let MongoClient = require('mongodb').MongoClient;
+
 // Package to parse http response bodies
 let bodyParser = require('body-parser');
 
@@ -14,40 +16,73 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-// If the Node.js app is started directly on the host one has to use localhost:port to reach the database
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
-/* If the Node.js app is started as a docker container within the same docker network as the database,
- * then the container name can be used instead of localhost:port.
- * This is the default, when using docker compose to build all these services using the docker-compose.yml config file
- */
-let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
-
-
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
- // Database as specified in the Mongo Express UI
-let dbName = "user-account";
-let collectionName = "users";
-
-
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
-app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+app.get('/api/profile-picture', function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, `images/${req.query.user}.jpg`));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
-// Add further get and post requests to fetch and post data from and to the database
-app.get('/profile', function(req, res){
+function profileDataComplete(profileData) {
+  // Checks whether the user data is complete
+  return (profileData && profileData.user && profileData.name && profileData.mail && profileData.interests)
+}
 
+// Add further get and post requests to fetch and post data from and to the database
+app.get('/api/profile', async function(req, res){
+  // DB
+  try {
+    let user = req.query.user;
+    if (!user) throw new Error("Invalid user");
+    let db = await getDB();
+    let userCol = db.collection(userColName);
+  
+    let profileData = await userCol.findOne({user: user});
+    if(!profileDataComplete(profileData)) {
+      throw new Error("Unkown user");
+    }
+    
+    res.end(JSON.stringify(profileData));
+  } catch(e) {
+    res.end(JSON.stringify({
+      err: e
+    }))
+    console.error(e)
+  }
 });
 
 
-app.post('/profile', function(req, res){
-  let db = cl
+app.post('/api/profile', async function(req, res){
+  
+  try {
+    // Data to insert
+    let profileData = req.body;
+    if(!profileDataComplete(profileData)) {
+      throw new Error("Incomplete profile data");
+    }
+    // DB
+    let db = await getDB();
+    let userCol = db.collection(userColName);
+    let updateRes = await userCol.updateOne({user: profileData.user}, { $set: profileData }, {upsert: true})
+    console.log(updateRes)
+    if (updateRes.modifiedCount !== 1 && updateRes.upsertedCount) {
+      throw new Error("Unable to write to db")
+    }
+    
+    // Return success if stored successfully
+    res.end(JSON.stringify({
+      "success": true
+    }));
+  } catch(e) {
+    res.end(JSON.stringify({
+      err: e
+    }))
+    console.error(e)
+  }
+  
 });
 
 
